9a43041b2ce5a59e9014ba3e2021fcc8a4d73751
William Marques <wmarques.pro@gmail.com>
2021-11-05 14:32:15 +0100

    add validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {of} from "rxjs";
+import {delay} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+
+  validatePhone() {
+    return of(true).pipe(delay(1000))
+  }
 }
