bf14e736ee3986df754eca3152cff6aca4eb0a07
William Marques <wmarques.pro@gmail.com>
2021-11-05 15:55:20 +0100

    step 1 - create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {of} from "rxjs";
 import {delay} from "rxjs/operators";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phoneNumber: [''],
+    });
+  }
 
   validatePhone() {
     return of(true).pipe(delay(1000))
+  }
+
+  submit() {
+    console.log(this.formGroup.value);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,9 +16,9 @@ import {MatButtonModule} from "@angular/material/button";
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
